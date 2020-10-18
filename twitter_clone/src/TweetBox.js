5e59ed90eb3714c71b1ba4df1d2d43ac246a3261
PoojaSharma291 <pooja.bpit09@gmail.com>
2020-10-19 01:38:08 +0530

    Adding Post for tweet

--- twitter_clone/src/TweetBox.js
+++ twitter_clone/src/TweetBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TweetBox.css';
 import { Avatar, Button } from '@material-ui/core';
 import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
@@ -6,16 +6,35 @@ import GifOutlinedIcon from '@material-ui/icons/GifOutlined';
 import PollOutlinedIcon from '@material-ui/icons/PollOutlined';
 import SentimentSatisfiedOutlinedIcon from '@material-ui/icons/SentimentSatisfiedOutlined';
 import ScheduleOutlinedIcon from '@material-ui/icons/ScheduleOutlined';
+import db from './firebase';
 
 function TweetBox(props) {
+    const [tweetMessage, setTweetMessage] = useState("");
+    const [tweetImage, setTweetImage] = useState("");
+    const sendTweet = e => {
+        e.preventDefault();
+        db.collection("posts").add({
+            displayName: "Pooja Gogia",
+            username: "sgogia",
+            verified: true,
+            text: tweetMessage,
+            image: tweetImage,
+            avatar: "assets/images/sg.jpg"
+        });
+
+        setTweetMessage("");
+        setTweetImage("");
+    }
     return (
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
                     <Avatar src="assets/images/codersCookbook.jpg">PS</Avatar>
-                    <input placeholder="What's happening?" type="text"></input>
+                    <input onChange={e => setTweetMessage(e.target.value)}
+                        value={tweetMessage} placeholder="What's happening?" type="text"></input>
                 </div>
-                <input className="tweetBox__imageInput" placeholder="Optional: Enter image URL" type="text" />
+                <input onChange={e => setTweetImage(e.target.value)} value={tweetImage}
+                    className="tweetBox__imageInput" placeholder="Optional: Enter image URL" type="text" />
                 <div className="tweetBox_buttonContainer">
                     <span className="tweetBox_buttonIcons">
                         <ImageOutlinedIcon fontSize="small" />
@@ -24,7 +43,7 @@ function TweetBox(props) {
                         <SentimentSatisfiedOutlinedIcon fontSize="small" />
                         <ScheduleOutlinedIcon fontSize="small" />
                     </span>
-                    <Button className="tweetBox__tweetButton">Tweet</Button>
+                    <Button onClick={sendTweet} type="submit" className="tweetBox__tweetButton">Tweet</Button>
                 </div>
 
 
